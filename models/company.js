9673ad216d922794cdddd47df95347aaded69d95
Nurul Fikri <58716824+nf0297@users.noreply.github.com>
2020-01-23 21:19:03 +0700

    Update company.js

--- models/company.js
+++ models/company.js
@@ -7,6 +7,12 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   company.associate = function(models) {
     // associations can be defined here
+    // if its HasMany then the foreignKey is from other table
+    // if its belongsTo then the foreignKey is from this table
+    company.hasMany(models.talent, {
+      as: "Talent",
+      foreignKey: "id_company"
+    })
   };
   return company;
 };
