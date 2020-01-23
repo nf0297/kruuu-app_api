a44b3c54dc4845201221e2f1014adf978c08b057
Nurul Fikri <58716824+nf0297@users.noreply.github.com>
2020-01-23 21:46:22 +0700

    Update 20200122141930-seed-job.js

--- seeders/20200122141930-seed-job.js
+++ seeders/20200122141930-seed-job.js
@@ -6,7 +6,7 @@ module.exports = {
       {
        title:'Headshot',
        image:'https://upload.wikimedia.org/wikipedia/en/thumb/6/6c/Headshot_%282016_film%29.jpg/220px-Headshot_%282016_film%29.jpg',
-       id_talent:'3',
+       id_talent:3,
        launch_date:'2016-12-08',
        is_done:true,
        createdAt:new Date,
@@ -15,7 +15,7 @@ module.exports = {
       {
        title:'3 Heroines',
        image:'https://img.reelgood.com/content/movie/bbde6920-11ae-40e1-90a4-77e458cdd223/poster-780.jpg',
-       id_talent:'3',
+       id_talent:3,
        launch_date:'2016-08-06',
        is_done:true,
        createdAt:new Date,
@@ -24,7 +24,7 @@ module.exports = {
       {
        title:'Dream High',
        image:'https://img.reelgood.com/content/show/2f579eb2-cea5-4dd0-a981-4dac2bbd0ab9/poster-780.jpg',
-       id_talent:'2',
+       id_talent:2,
        launch_date:'2011-01-03',
        is_done:true,
        createdAt:new Date,
@@ -33,7 +33,7 @@ module.exports = {
       {
        title:'Hotel Del Luna',
        image:'https://www.hancinema.net/photos/fullsizephoto1080037.jpg',
-       id_talent:'2',
+       id_talent:2,
        launch_date:'2019-07-13',
        is_done:true,
        createdAt:new Date,
@@ -42,7 +42,7 @@ module.exports = {
       {
         title:'Moon Lovers: Scarlet Heart Ryeo',
         image:'http://www.koreandrama.org/wp-content/uploads/2016/08/Scarlet-Heart-Ryeo-03.jpg',
-        id_talent:'2',
+        id_talent:2,
         launch_date:'2016-08-29',
         is_done:true,
         createdAt:new Date,
@@ -51,7 +51,7 @@ module.exports = {
       {
         title:'The Witcher',
         image:'https://gamespot1.cbsistatic.com/uploads/scale_super/1582/15828986/3606754-witcher%20poster.jpg',
-        id_talent:'4',
+        id_talent:4,
         launch_date:'2019-12-20',
         is_done:true,
         createdAt:new Date,
@@ -60,7 +60,7 @@ module.exports = {
       {
         title:'Man of Steel',
         image:'https://images-na.ssl-images-amazon.com/images/I/71j%2BjX%2BnBBL._AC_SY679_.jpg',
-        id_talent:'5',
+        id_talent:5,
         launch_date:'2013-06-13',
         is_done:true,
         createdAt:new Date,
@@ -69,7 +69,7 @@ module.exports = {
       {
         title:'Batman v Superman',
         image:'https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
-        id_talent:'5',
+        id_talent:5,
         launch_date:'2016-03-23',
         is_done:true,
         createdAt:new Date,
